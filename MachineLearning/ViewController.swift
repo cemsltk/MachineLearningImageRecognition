8f6fb4c07c9871d9553573216b80b1b74ebfe181
Cem Saltık <cemsltk35@gmail.com>
2021-03-26 13:32:05 +0300

    Initial Commit

---
+++ MachineLearning/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MachineLearning
+//
+//  Created by Cem Saltık on 26.03.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
